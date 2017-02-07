ca1f896ece4251630ae3e065dfdc6004d7b4edb1
Binoy Jayan <binoy.jayan@linaro.org>
2017-02-07 13:24:21 +0530

    utils: Added usage

--- utils/qemu_aarch64.sh
+++ utils/qemu_aarch64.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+BASESTR=`basename $0`
+usage()
+{
+	echo ""
+	echo "Usage:"
+	echo ""
+	echo "$BASESTR -h : Display this help message"
+	echo "$BASESTR -g : Run qemu in debug mode by listening on tcp port 1234"
+	echo ""
+	echo "While running in debug mode, connect using a gdb client as:"
+	echo "  aarch64-linux-gnu-gdb vmlinux"
+	echo "  target remote localhost:1234"
+	echo ""
+}
+
 DBG=""
 KLOC="/local/mnt/workspace/src/korg/linux_arm64"
 RLOC="/local/mnt/workspace/images/rootfs/arm64"
@@ -12,16 +27,27 @@ K=$KLOC/$KERNEL
 R=$RLOC/$DISKA
 D=$RLOC/$DISKB
 
+
+if [ "$1" == '-h' ]
+then
+	usage
+	exit
+elif [ "$1" == '-g' ]
+then
+	DBG="-s -S"
+	echo "Running qemu in debug mode [$DBG]. Connect using a gdb client as:"
+	echo ""
+	echo "aarch64-linux-gnu-gdb vmlinux"
+	echo "target remote localhost:1234"
+	echo ""
+fi
+
 echo ""
 echo "KERNEL : $K"
 echo "ROOTFS : $R"
 echo "DISK2  : $D"
 echo ""
 
-if [ "$1" == '-g' ]
-then
-	DBG="-s -S"
-fi
 
 qemu-system-aarch64 \
  -machine virt \
