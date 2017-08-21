f2339d72e4abc7902afe57c1b6e1d750ada1ed0c
Binoy Jayan <bjayan@codeaurora.org>
2017-08-21 13:55:29 +0530

    Remove unwanted files
    Signed-off-by: Binoy Jayan <bjayan@codeaurora.org>

--- utils/myco.sh
+++
@@ -1,4 +0,0 @@
-
-git clone blr-ubuntu-59:/local/mnt/workspace/src/backup/kernel.git
-
-
--- utils/sys.sh
+++
@@ -1,9 +0,0 @@
-
-echo "Host name  : blr-ubuntu-59"
-echo "IP Address : 10.79.40.235"
-
-echo "Lab machine  : blr-ubuntu-38"
-
-echo "Jaiju's machine  : blr-ubuntu-73"
-
-
