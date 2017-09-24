64f017eca36f2685629c25ad2d8367c754223b33
Binoy Jayan <binoy.jayan@linaro.org>
2017-09-25 04:49:37 +0530

    gread: Display GUID type names instead of values
    Display the names of the well known GUID partition types.

--- gptdisk/gread.c
+++ gptdisk/gread.c
@@ -5,8 +5,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#define PACKED __attribute__ ((packed))
+#define PACKED __attribute__  ((packed))
 #define LOGICAL_BLOCK_SIZE    512
+#define ARRAY_SIZE(x)         (sizeof(x) / sizeof((x)[0]))
 
 struct gpt_header {
 	char signature[8];
@@ -61,7 +62,139 @@ struct options {
 	 * GPT headers are saved by utilities like gdisk. This does not
 	 */
 	int saved;
+	int guid;
 	const char *filename;
+};
+
+// GUID partition type definition
+struct part_type {
+	const uint16_t mbr_type;
+	const char *guid_type;
+	const char *name;
+};
+
+// GUID partition types
+static const struct part_type part_types[] = {
+	0x0000, "00000000-0000-0000-0000-000000000000", "Unused entry",
+	0x0100, "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", "Microsoft basic data", // FAT-12
+	0x0400, "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", "Microsoft basic data", // FAT-16 < 32M
+	0x0600, "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", "Microsoft basic data", // FAT-16
+	0x0700, "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", "Microsoft basic data", // NTFS (or HPFS)
+	0x0b00, "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", "Microsoft basic data", // FAT-32
+	0x0c00, "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", "Microsoft basic data", // FAT-32 LBA
+	0x0c01, "E3C9E316-0B5C-4DB8-817D-F92DF00215AE", "Microsoft reserved",
+	0x0e00, "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", "Microsoft basic data", // FAT-16 LBA
+	0x1100, "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", "Microsoft basic data", // Hidden FAT-12
+	0x1400, "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", "Microsoft basic data", // Hidden FAT-16 < 32M
+	0x1600, "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", "Microsoft basic data", // Hidden FAT-16
+	0x1700, "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", "Microsoft basic data", // Hidden NTFS (or HPFS)
+	0x1b00, "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", "Microsoft basic data", // Hidden FAT-32
+	0x1c00, "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", "Microsoft basic data", // Hidden FAT-32 LBA
+	0x1e00, "EBD0A0A2-B9E5-4433-87C0-68B6B72699C7", "Microsoft basic data", // Hidden FAT-16 LBA
+	0x2700, "DE94BBA4-06D1-4D40-A16A-BFD50179D6AC", "Windows RE",
+	0x3000, "7412F7D5-A156-4B13-81DC-867174929325", "ONIE boot",
+	0x3001, "D4E6E2CD-4469-46F3-B5CB-1BFF57AFC149", "ONIE config",
+	0x3900, "C91818F9-8025-47AF-89D2-F030D7000C2C", "Plan 9",
+	0x4100, "9E1A2D38-C612-4316-AA26-8B49521E5A8B", "PowerPC PReP boot",
+	0x4200, "AF9B60A0-1431-4F62-BC68-3311714A69AD", "Windows LDM data", // Logical disk manager
+	0x4201, "5808C8AA-7E8F-42E0-85D2-E1E90434CFB3", "Windows LDM metadata", // Logical disk manager
+	0x4202, "E75CAF8F-F680-4CEE-AFA3-B001E56EFC2D", "Windows Storage Spaces", // A newer LDM-type setup
+	0x7501, "37AFFC90-EF7D-4E96-91C3-2D7AE055B174", "IBM GPFS", // General Parallel File System (GPFS)
+	0x7f00, "FE3A2A5D-4F32-41A7-B725-ACCC3285A309", "ChromeOS kernel",
+	0x7f01, "3CB8E202-3B7E-47DD-8A3C-7FF2A13CFCEC", "ChromeOS root",
+	0x7f02, "2E0A753D-9E48-43B0-8337-B15192CB1B5E", "ChromeOS reserved",
+	0x8200, "0657FD6D-A4AB-43C4-84E5-0933C84B4F4F", "Linux swap", // Linux swap (or Solaris on MBR)
+	0x8300, "0FC63DAF-8483-4772-8E79-3D69D8477DE4", "Linux filesystem", // Linux native
+	0x8301, "8DA63339-0007-60C0-C436-083AC8230908", "Linux reserved",
+	0x8302, "933AC7E1-2EB4-4F13-B844-0E14E2AEF915", "Linux /home", // Linux /home (auto-mounted by systemd)
+	0x8303, "44479540-F297-41B2-9AF7-D131D5F0458A", "Linux x86 root (/)", // Linux / on x86 (auto-mounted by systemd)
+	0x8304, "4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709", "Linux x86-64 root (/)", // Linux / on x86-64 (auto-mounted by systemd)
+	0x8305, "B921B045-1DF0-41C3-AF44-4C6F280D3FAE", "Linux ARM64 root (/)", // Linux / on 64-bit ARM (auto-mounted by systemd)
+	0x8306, "3B8F8425-20E0-4F3B-907F-1A25A76F98E8", "Linux /srv", // Linux /srv (auto-mounted by systemd)
+	0x8307, "69DAD710-2CE4-4E3C-B16C-21A1D49ABED3", "Linux ARM32 root (/)", // Linux / on 32-bit ARM (auto-mounted by systemd)
+	0x8400, "D3BFE2DE-3DAF-11DF-BA40-E3A556D89593", "Intel Rapid Start",
+	0x8e00, "E6D6D379-F507-44C2-A23C-238F2A3DF928", "Linux LVM",
+	0xa000, "2568845D-2332-4675-BC39-8FA5A4748D15", "Android bootloader",
+	0xa001, "114EAFFE-1552-4022-B26E-9B053604CF84", "Android bootloader 2",
+	0xa002, "49A4D17F-93A3-45C1-A0DE-F50B2EBE2599", "Android boot",
+	0xa003, "4177C722-9E92-4AAB-8644-43502BFD5506", "Android recovery",
+	0xa004, "EF32A33B-A409-486C-9141-9FFB711F6266", "Android misc",
+	0xa005, "20AC26BE-20B7-11E3-84C5-6CFDB94711E9", "Android metadata",
+	0xa006, "38F428E6-D326-425D-9140-6E0EA133647C", "Android system",
+	0xa007, "A893EF21-E428-470A-9E55-0668FD91A2D9", "Android cache",
+	0xa008, "DC76DDA9-5AC1-491C-AF42-A82591580C0D", "Android data",
+	0xa009, "EBC597D0-2053-4B15-8B64-E0AAC75F4DB1", "Android persistent",
+	0xa00a, "8F68CC74-C5E5-48DA-BE91-A0C8C15E9C80", "Android factory",
+	0xa00b, "767941D0-2085-11E3-AD3B-6CFDB94711E9", "Android fastboot/tertiary",
+	0xa00c, "AC6D7924-EB71-4DF8-B48D-E267B27148FF", "Android OEM",
+	0xa500, "516E7CB4-6ECF-11D6-8FF8-00022D09712B", "FreeBSD disklabel",
+	0xa501, "83BD6B9D-7F41-11DC-BE0B-001560B84F0F", "FreeBSD boot",
+	0xa502, "516E7CB5-6ECF-11D6-8FF8-00022D09712B", "FreeBSD swap",
+	0xa503, "516E7CB6-6ECF-11D6-8FF8-00022D09712B", "FreeBSD UFS",
+	0xa504, "516E7CBA-6ECF-11D6-8FF8-00022D09712B", "FreeBSD ZFS",
+	0xa505, "516E7CB8-6ECF-11D6-8FF8-00022D09712B", "FreeBSD Vinum/RAID",
+	0xa580, "85D5E45A-237C-11E1-B4B3-E89A8F7FC3A7", "Midnight BSD data",
+	0xa581, "85D5E45E-237C-11E1-B4B3-E89A8F7FC3A7", "Midnight BSD boot",
+	0xa582, "85D5E45B-237C-11E1-B4B3-E89A8F7FC3A7", "Midnight BSD swap",
+	0xa583, "0394Ef8B-237E-11E1-B4B3-E89A8F7FC3A7", "Midnight BSD UFS",
+	0xa584, "85D5E45D-237C-11E1-B4B3-E89A8F7FC3A7", "Midnight BSD ZFS",
+	0xa585, "85D5E45C-237C-11E1-B4B3-E89A8F7FC3A7", "Midnight BSD Vinum",
+	0xa600, "824CC7A0-36A8-11E3-890A-952519AD3F61", "OpenBSD disklabel",
+	0xa800, "55465300-0000-11AA-AA11-00306543ECAC", "Apple UFS", // Mac OS X
+	0xa900, "516E7CB4-6ECF-11D6-8FF8-00022D09712B", "FreeBSD disklabel", // NetBSD disklabel
+	0xa901, "49F48D32-B10E-11DC-B99B-0019D1879648", "NetBSD swap",
+	0xa902, "49F48D5A-B10E-11DC-B99B-0019D1879648", "NetBSD FFS",
+	0xa903, "49F48D82-B10E-11DC-B99B-0019D1879648", "NetBSD LFS",
+	0xa904, "2DB519C4-B10F-11DC-B99B-0019D1879648", "NetBSD concatenated",
+	0xa905, "2DB519EC-B10F-11DC-B99B-0019D1879648", "NetBSD encrypted",
+	0xa906, "49F48DAA-B10E-11DC-B99B-0019D1879648", "NetBSD RAID",
+	0xab00, "426F6F74-0000-11AA-AA11-00306543ECAC", "Recovery HD",
+	0xaf00, "48465300-0000-11AA-AA11-00306543ECAC", "Apple HFS/HFS+",
+	0xaf01, "52414944-0000-11AA-AA11-00306543ECAC", "Apple RAID",
+	0xaf02, "52414944-5F4F-11AA-AA11-00306543ECAC", "Apple RAID offline",
+	0xaf03, "4C616265-6C00-11AA-AA11-00306543ECAC", "Apple label",
+	0xaf04, "5265636F-7665-11AA-AA11-00306543ECAC", "AppleTV recovery",
+	0xaf05, "53746F72-6167-11AA-AA11-00306543ECAC", "Apple Core Storage",
+	0xaf06, "B6FA30DA-92D2-4A9A-96F1-871EC6486200", "Apple SoftRAID Status",
+	0xaf07, "2E313465-19B9-463F-8126-8A7993773801", "Apple SoftRAID Scratch",
+	0xaf08, "FA709C7E-65B1-4593-BFD5-E71D61DE9B02", "Apple SoftRAID Volume",
+	0xaf09, "BBBA6DF5-F46F-4A89-8F59-8765B2727503", "Apple SoftRAID Cache",
+	0xb300, "CEF5A9AD-73BC-4601-89F3-CDEEEEE321A1", "QNX6 Power-Safe",
+	0xbc00, "0311FC50-01CA-4725-AD77-9ADBB20ACE98", "Acronis Secure Zone",
+	0xbe00, "6A82CB45-1DD2-11B2-99A6-080020736631", "Solaris boot",
+	0xbf00, "6A85CF4D-1DD2-11B2-99A6-080020736631", "Solaris root",
+	0xbf01, "6A898CC3-1DD2-11B2-99A6-080020736631", "Solaris /usr & Mac ZFS", // Solaris/MacOS
+	0xbf02, "6A87C46F-1DD2-11B2-99A6-080020736631", "Solaris swap",
+	0xbf03, "6A8B642B-1DD2-11B2-99A6-080020736631", "Solaris backup",
+	0xbf04, "6A8EF2E9-1DD2-11B2-99A6-080020736631", "Solaris /var",
+	0xbf05, "6A90BA39-1DD2-11B2-99A6-080020736631", "Solaris /home",
+	0xbf06, "6A9283A5-1DD2-11B2-99A6-080020736631", "Solaris alternate sector",
+	0xbf07, "6A945A3B-1DD2-11B2-99A6-080020736631", "Solaris Reserved 1",
+	0xbf08, "6A9630D1-1DD2-11B2-99A6-080020736631", "Solaris Reserved 2",
+	0xbf09, "6A980767-1DD2-11B2-99A6-080020736631", "Solaris Reserved 3",
+	0xbf0a, "6A96237F-1DD2-11B2-99A6-080020736631", "Solaris Reserved 4",
+	0xbf0b, "6A8D2AC7-1DD2-11B2-99A6-080020736631", "Solaris Reserved 5",
+	0xc001, "75894C1E-3AEB-11D3-B7C1-7B03A0000000", "HP-UX data",
+	0xc002, "E2A1E728-32E3-11D6-A682-7B03A0000000", "HP-UX service",
+	0xe100, "7412F7D5-A156-4B13-81DC-867174929325", "ONIE boot",
+	0xe101, "D4E6E2CD-4469-46F3-B5CB-1BFF57AFC149", "ONIE config",
+	0xea00, "BC13C2FF-59E6-4262-A352-B275FD6F7172", "Freedesktop $BOOT",
+	0xeb00, "42465331-3BA3-10F1-802A-4861696B7521", "Haiku BFS",
+	0xed00, "F4019732-066E-4E12-8273-346C5641494F", "Sony system partition",
+	0xed01, "BFBFAFE7-A34F-448A-9A5B-6213EB736C22", "Lenovo system partition",
+	0xef00, "C12A7328-F81F-11D2-BA4B-00A0C93EC93B", "EFI System", // Parted identifies these as having the "boot flag" set
+	0xef01, "024DEE41-33E7-11D3-9D69-0008C781F39F", "MBR partition scheme", // Used to nest MBR in GPT
+	0xef02, "21686148-6449-6E6F-744E-656564454649", "BIOS boot partition", // Used by GRUB
+	0xf800, "4FBD7E29-9D25-41B8-AFD0-062C0CEFF05D", "Ceph OSD", // Ceph Object Storage Daemon
+	0xf801, "4FBD7E29-9D25-41B8-AFD0-5EC00CEFF05D", "Ceph dm-crypt OSD", // Ceph Object Storage Daemon (encrypted)
+	0xf802, "45B0969E-9B03-4F30-B4C6-B4B80CEFF106", "Ceph journal",
+	0xf803, "45B0969E-9B03-4F30-B4C6-5EC00CEFF106", "Ceph dm-crypt journal",
+	0xf804, "89C57F98-2FE5-4DC0-89C1-F3AD0CEFF2BE", "Ceph disk in creation",
+	0xf805, "89C57F98-2FE5-4DC0-89C1-5EC00CEFF2BE", "Ceph dm-crypt disk in creation",
+	0xfb00, "AA31E02A-400F-11DB-9590-000C2911D1B8", "VMWare VMFS",
+	0xfb01, "9198EFFC-31C0-11DB-8F78-000C2911D1B8", "VMWare reserved",
+	0xfc00, "9D275380-40AD-11DB-BF97-000C2911D1B8", "VMWare kcore crash protection",
+	0xfd00, "A19D880F-05FC-4D3B-A006-743F0F84911E", "Linux RAID"
 };
 
 static const uint32_t crc32_tab[] = { 0x00000000L, 0x77073096L, 0xee0e612cL,
@@ -255,10 +388,26 @@ int disp_gpt_header(int fd, struct gpt_header * gpt, char *which)
 	return 0;
 }
 
-void print_gpt_partitions(int fd, const struct gpt_header * gpt, char *which)
+const struct part_type* guid_lookup(char *g)
 {
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(part_types); i++) {
+		if (!strcmp(part_types[i].guid_type, g))
+			return &part_types[i];
+	}
+	return NULL;
+}
+
+void print_gpt_partitions(int fd, const struct gpt_header * gpt, char *which, int guid)
+{
+	char *tmp;
+	const char *gstr;
+	const struct part_type* ptype;
+	int i = 0;
 	int entries_size = gpt->entry_count * gpt->entry_size;
-	char * buf = (char *) malloc(entries_size);
+	char *buf = (char *) malloc(entries_size);
+
 	read(fd, buf, entries_size);
 	wchar_t *gpt_entry_name;
 	struct part_entry *gentry = (struct part_entry *) buf;
@@ -268,8 +417,6 @@ void print_gpt_partitions(int fd, const struct gpt_header * gpt, char *which)
 	printf("Parition entries [%s]\n", which);
 	printf("-----------------------------------------------------------------------------------------------------------------------------\n");
 
-	char * tmp;
-	int i = 0;
 	printf("%-37s %-37s %-10s %-10s %-6s %s", "Type GUID", "Unique GUID", "LBA First", "LBA Last", "Attr", "Name\n");
 	printf("-----------------------------------------------------------------------------------------------------------------------------\n");
 	for (; i < gpt->entry_count; i++) {
@@ -277,7 +424,15 @@ void print_gpt_partitions(int fd, const struct gpt_header * gpt, char *which)
 			continue;
 		gpt_entry_name = utf16_to_wchar(gentry->name, 36);
 		tmp = uuid_to_str(gentry->type_guid);
-		printf("%-37s ", tmp);
+		if (guid) {
+			printf("%-37s ", tmp);
+		} else {
+			ptype = guid_lookup(tmp);
+			if (ptype)
+				printf("0x%04hx %-30s ", ptype->mbr_type, ptype->name);
+			else
+				printf("%-37s ", tmp);
+		}
 		free(tmp);
 		tmp = uuid_to_str(gentry->unique_guid);
 		printf("%-37s ", tmp);
@@ -298,13 +453,15 @@ void print_gpt_partitions(int fd, const struct gpt_header * gpt, char *which)
 }
 
 const char *usage_str = "\n" \
-	"Usage: gread [-h|--help] [-b|--backup] [-m|--mbr] [-s|--saved] <device file>\n\n" \
+	"Usage: gread [-h|--help] [-b|--backup] [-m|--mbr] [-s|--saved] [-g|--guid] <device file>\n\n" \
 	"--mbr     : Displays legacy-mbr headers\n"\
 	"--backup  : Displays backup headers and partition table instead of primary\n"\
+	"--guid    : Display the GUID type values instead of names\n"\
 	"--saved   : Display the GPT data saved in a file\n"\
 	"\nExamples:\n\n"\
 	"  gread -m /dev/sda\n" \
 	"  gread -b /dev/sda\n" \
+	"  gread -g /dev/sda\n" \
 	"  gread -s gpt_data.bin\n";
 
 static int is_arg(const char *arg, const char *ch1, const char *ch2)
@@ -321,6 +478,7 @@ int get_options(int argc, const char * argv[], struct options *o)
 	o->primary = 1;
 	o->saved = 0;
 	o->legacy = 0;
+	o->guid = 0;
 	o->filename = NULL;
 
 	if (argc < 2) {
@@ -339,6 +497,8 @@ int get_options(int argc, const char * argv[], struct options *o)
 				o->primary = 0;
 			else if (is_arg(argv[i], "-m", "--mbr"))
 				o->legacy = 1;
+			else if (is_arg(argv[i], "-g", "--guid"))
+				o->guid = 1;
 			else if (is_arg(argv[i], "-s", "--saved"))
 				o->saved = 1;
 			else {
@@ -365,7 +525,7 @@ int get_options(int argc, const char * argv[], struct options *o)
 
 static void lba_align(const char *s, int fd, long pos, int align)
 {
-	if (pos % 512) {
+	if (pos % LOGICAL_BLOCK_SIZE) {
 		printf("\nThe device does not end in a valid LBA boundary\n");
 		pos = (pos / align) * align;
 		printf("Aligning the %s to %ld [LBA %ld]\n\n", s, pos, pos/align);
@@ -400,7 +560,7 @@ int main(int argc, const char * argv[])
 			goto end;
 		if (disp_gpt_header(fd, &header, "Backup"))
 			goto end;
-		print_gpt_partitions(fd, &header, "common");
+		print_gpt_partitions(fd, &header, "common", o.guid);
 		goto end;
 	}
 
@@ -408,7 +568,7 @@ int main(int argc, const char * argv[])
 	if (o.primary) {
 		if (disp_gpt_header(fd, &header, "Primary"))
 			goto end;
-		print_gpt_partitions(fd, &header, "Primary");
+		print_gpt_partitions(fd, &header, "Primary", o.guid);
 		goto end;
 	}
 
@@ -419,7 +579,7 @@ int main(int argc, const char * argv[])
 		goto end;
 	}
 
-	lba_align("header", fd, n, 512);
+	lba_align("header", fd, n, LOGICAL_BLOCK_SIZE);
 	disp_gpt_header(fd, &header, "Backup");
 
 	// Seek to the backup partition entries
@@ -429,8 +589,8 @@ int main(int argc, const char * argv[])
 		exitCode = EXIT_FAILURE;
 		goto end;
 	}
-	lba_align("partition table", fd, n, 512);
-	print_gpt_partitions(fd, &header, "Backup");
+	lba_align("partition table", fd, n, LOGICAL_BLOCK_SIZE);
+	print_gpt_partitions(fd, &header, "Backup", o.guid);
 
 end:
 	close(fd);
