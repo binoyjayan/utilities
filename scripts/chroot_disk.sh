2dc4d1ef5dde73e4921bb283a86fa9806506c665
Binoy <binoyjayan@gmail.com>
2019-12-18 12:15:02 -0800

    add script to chroot disk image

--- scripts/chroot_disk.sh
+++ scripts/chroot_disk.sh
@@ -1,28 +1,138 @@
 #!/bin/bash
 
+BASESTR=`basename $0`
+IMG=cstor10-installer-1804.img
+MOUNTDIR=/mnt/disk
 BLOCK=2048
 BS=512
 OFFSET=`expr $BLOCK \* $BS`
-echo "Mounting file system at offset $OFFSET [ block:$BLOCK bs:$BS ]"
+UMOUNTFLAG=false
+DNSFLAG=false
+DNS=/etc/resolv.conf
+HELPFLAG=false
 
-mkdir -p /mnt/disk
-mount -o loop,offset=$OFFSET disk.img /mnt/disk
+usage()
+{
+      echo ""
+      echo "Usage: $BASESTR <option> [parameter] [option2] [param2]..."
+      echo ""
+      echo "Options"
+      echo ""
+      echo "-f image   : Device name. Default: $IMG"
+      echo "-d dir     : mount directory. Default: $MOUNTDIR"
+      echo "-o offset  : Offset on device to mount from. Default: $OFFSET"
+      echo "-u         : unmount all directories on chroot-ed filesystem"
+      echo "-D         : Copy the host dns settings"
+      echo "-h         : Display usage"
+      echo ""
+      echo "Examples:"
+      echo ""
+      echo "$BASESTR"
+      echo "$BASESTR -f disk.img"
+      echo "$BASESTR -f disk.img -o $OFFSET"
+      echo "$BASESTR -f disk.img -o $OFFSET -d /mnt/rootdisk"
+      echo ""
+}
 
-echo sudo mount -t proc proc /mnt/disk/proc
-echo sudo mount -t sysfs sys /mnt/disk/sys
-echo sudo mount -o bind /dev /mnt/disk/dev
+check_su()
+{
+	ME=`whoami`
+	if [ $ME != "root" ]; then
+		echo "Run the script as a root"
+		echo ""
+		exit
+	fi
+}
 
-echo sudo mv /mnt/disk/etc/resolv.conf /mnt/disk/etc/resolv.conf.bkp
-echo sudo cp -L /etc/resolv.conf /mnt/disk/etc/resolv.conf
-echo sudo chroot /mnt/disk /bin/bash
+# Main script execution
+echo ""
+while getopts "f:d:o:uDh" opt; do
+  case $opt in
+    f)
+        IMG=$OPTARG
+        ;;
+    d)
+        MOUNTDIR=$OPTARG
+        ;;
+    o)
+        OFFSET=$OPTARG
+        ;;
+    u)
+        UMOUNTFLAG=true
+        ;;
+    D)
+	DNSFLAG=true
+        ;;
+    h)
+	HELPFLAG=true
+        ;;
+    \?)
+        echo ""
+        echo "Invalid/insufficient arguments mentioned!"
+        usage
+	exit
+        ;;
+  esac
+done
 
+if [ "$HELPFLAG" == "true" ]; then
+	usage
+	exit
+fi
 
-echo "cleanup..."
-echo sudo rm /mnt/disk/etc/resolv.conf
-echo sudo mv /mnt/disk/etc/resolv.conf.bkp /mnt/disk/etc/resolv.conf
+check_su
 
-echo sudo umount /mnt/disk/dev
-echo sudo umount /mnt/disk/sys
-echo sudo umount /mnt/disk/proc
-echo sudo umount /mnt/disk
+if [ ! -f "$IMG" ]; then
+	echo "please specify a valid image file using option -f"
+	exit
+fi
+
+if [ "$UMOUNTFLAG" == "false" ]; then
+	if mountpoint "$MOUNTDIR" 2>&1 > /dev/null; then
+        	echo "$MOUNTDIR is already mounted. Please unmount or use another directory"
+		exit
+	fi
+
+	echo "Mounting file system on [ $IMG ] at offset $OFFSET" at "$MOUNTDIR"
+
+	mkdir -p $MOUNTDIR
+
+	mount -o loop,offset=$OFFSET $IMG $MOUNTDIR
+	mount -t proc proc ${MOUNTDIR}/proc
+	mount -t sysfs sys ${MOUNTDIR}/sys
+	mount -o bind /dev ${MOUNTDIR}/dev
+
+	if [ "$DNSFLAG" == "true" ]; then
+		DNS_CURR=${MOUNTDIR}${DNS}
+		DNS_ORIG=${MOUNTDIR}${DNS}.orig
+		echo "Copying DNS settings [ backup: $DNS_ORIG ]..."
+		mv ${MOUNTDIR}${DNS} ${MOUNTDIR}${DNS}.orig
+		cp -L $DNS ${MOUNTDIR}${DNS}
+	fi
+
+	echo ""
+	echo "Run the following command to enter chroot session"
+	echo "Type exit when finished and use the option -u to unmount all directories"
+	echo ""
+	echo "chroot ${MOUNTDIR} /bin/bash"
+	echo ""
+else
+	if [ "$DNSFLAG" == "true" ]; then
+		if [ ! -f "${MOUNTDIR}${DNS}.orig" ]; then
+			echo "No DNS restore file found"
+		else
+			echo "Restoring DNS settings from $DNS_ORIG..."
+			rm ${MOUNTDIR}${DNS}
+			mv ${MOUNTDIR}${DNS}.orig ${MOUNTDIR}${DNS}
+		fi
+	fi
+
+	echo "unmounting..."
+
+	umount ${MOUNTDIR}/dev
+	umount ${MOUNTDIR}/sys
+	umount ${MOUNTDIR}/proc
+	umount ${MOUNTDIR}
+fi
+
 
